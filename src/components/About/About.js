df2f4335d8b6ae147462c48884d369b33f9870fd
Roshan KC <83297534+roshankcpkr@users.noreply.github.com>
2025-05-19 09:33:19 +0545

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ import './About.css'
 import { ThemeContext } from '../../contexts/theme'
 import roshanlight from './roshan_light.png'
 import roshandark from './roshan_dark.png'
-import repdf from "./roshanros.pdf"
+import repdf from "./resumeros.pdf"
 
 const About = () => {
   const { name, role, description, resume, social } = about
